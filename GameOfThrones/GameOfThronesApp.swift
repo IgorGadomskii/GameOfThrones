d0d2076d4227468e62de1660128e712904779446
Igor Gadomskii <igvgad@gmail.com>
2022-10-19 01:14:38 +0600

    Initial Commit

---
+++ GameOfThrones/GameOfThronesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfThronesApp.swift
+//  GameOfThrones
+//
+//  Created by Macbook on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct GameOfThronesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
